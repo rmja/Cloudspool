1896bb35c8d7e991d49c013906ee423a723b59ba
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-03-27 08:30:14 +0100

    Use InMemory database for tests

--- test/Api.Tests/Config.cs
+++ test/Api.Tests/Config.cs
@@ -2,6 +2,6 @@
 {
     public static class Config
     {
-        public static readonly DatabaseProvider TestDatabaseProvider = DatabaseProvider.Npgsql;
+        public static readonly DatabaseProvider TestDatabaseProvider = DatabaseProvider.InMemory;
     }
 }
