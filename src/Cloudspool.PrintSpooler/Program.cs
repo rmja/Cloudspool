8bc977471eadcf5a9c2fb9c7d92ad8df694cc8a5
Rasmus Melchior Jacobsen <rmja@laesoe.org>
2020-03-27 07:59:59 +0100

    Add updater to PrintSpooler

--- src/Cloudspool.PrintSpooler/Program.cs
+++ src/Cloudspool.PrintSpooler/Program.cs
@@ -1,14 +1,46 @@
+using Cloudspool.PrintSpooler.Proxy;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Cloudspool.PrintSpooler.Proxy;
+using Onova;
+using Onova.Models;
+using Onova.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace Cloudspool.PrintSpooler
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task<int> Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var updatee = AssemblyMetadata.FromEntryAssembly();
+
+            Console.WriteLine($"PrintSpooler v{updatee.Version}");
+
+            using (var um = new UpdateManager(
+                updatee,
+                new GithubPackageResolver("rmja", "Cloudspool", "PrintSpooler-*.zip"),
+                new ZipPackageExtractor()))
+            {
+                var check = await um.CheckForUpdatesAsync();
+
+                if (!check.CanUpdate)
+                {
+                    Console.WriteLine("There are no updates available");
+                }
+                else
+                {
+                    Console.WriteLine($"Preparing update to v{check.LastVersion}");
+
+                    await um.PrepareUpdateAsync(check.LastVersion);
+
+                    um.LaunchUpdater(check.LastVersion);
+                    return 1;
+                }
+            }
+
+            await CreateHostBuilder(args).Build().RunAsync();
+            return 0;
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
